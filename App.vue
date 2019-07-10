0232d7e2bdfc15b8023bcdae10a57984f8ed1955
ZC <chaohfut@163.com>
2019-07-11 00:13:49 +0800

    testcommit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <a href="page1.html">page1</a>
+    <a href="page2.html">page2</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
